a823b18dd08688ee5d76ab176dd6f4fd79331bc5
Fealerok <111392244+Fealerok@users.noreply.github.com>
2025-03-15 00:41:12 +0300

    Update knexfile.ts

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -1,24 +1,31 @@
-import type { Knex } from "knex";
-require("dotenv").config();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 
-//Объект с настройками конфигурации knex (данные БД и папка для миграций)
-//Создание файла миграций - npx knex migrate:make name_migrate
-const config: { [key: string]: Knex.Config} = {
+const config = {
     development: {
         client: "pg",
         connection: {
-          host: process.env.DB_HOST,
-          user: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
         },
         migrations: {
-          directory: "./src/migrations"
+            directory: "./src/migrations"
         },
         seeds: {
-          directory: "./src/seeds"
+            directory: "./src/seeds"
         },
-      },
-}
-
-export default config;
+    },
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL, // Используем переменную окружения DATABASE_URL
+        migrations: {
+            directory: "./src/migrations"
+        },
+        seeds: {
+            directory: "./src/seeds"
+        },
+    }
+};
+exports.default = config;
