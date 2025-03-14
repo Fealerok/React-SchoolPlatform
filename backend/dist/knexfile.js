b4899a1dd6573a604991483626d5e225b4127df3
Fealerok <111392244+Fealerok@users.noreply.github.com>
2025-03-15 00:27:56 +0300

    Update knexfile.js

--- backend/dist/knexfile.js
+++ backend/dist/knexfile.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-//Объект с настройками конфигурации knex (данные БД и папка для миграций)
-//Создание файла миграций - npx knex migrate:make name_migrate
+
 const config = {
     development: {
         client: "pg",
@@ -19,5 +17,15 @@ const config = {
             directory: "./src/seeds"
         },
     },
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL, // Используем переменную окружения DATABASE_URL
+        migrations: {
+            directory: "./src/migrations"
+        },
+        seeds: {
+            directory: "./src/seeds"
+        },
+    }
 };
 exports.default = config;
