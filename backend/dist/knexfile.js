67c006dc3e3df9b9d8842db4c95fbcd441b59102
Fealerok <111392244+Fealerok@users.noreply.github.com>
2025-03-15 00:46:30 +0300

    1

--- backend/dist/knexfile.js
+++ backend/dist/knexfile.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-
+require("dotenv").config();
+//Объект с настройками конфигурации knex (данные БД и папка для миграций)
+//Создание файла миграций - npx knex migrate:make name_migrate
 const config = {
     development: {
         client: "pg",
--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -1,23 +1,26 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import type { Knex } from "knex";
+require("dotenv").config();
 
-const config = {
+//Объект с настройками конфигурации knex (данные БД и папка для миграций)
+//Создание файла миграций - npx knex migrate:make name_migrate
+const config: { [key: string]: Knex.Config} = {
     development: {
         client: "pg",
         connection: {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME
         },
         migrations: {
-            directory: "./src/migrations"
+          directory: "./src/migrations"
         },
         seeds: {
-            directory: "./src/seeds"
+          directory: "./src/seeds"
         },
-    },
-    production: {
+      },
+
+      production: {
         client: "pg",
         connection: process.env.DATABASE_URL, // Используем переменную окружения DATABASE_URL
         migrations: {
@@ -27,5 +30,6 @@ const config = {
             directory: "./src/seeds"
         },
     }
-};
-exports.default = config;
+}
+
+export default config;
